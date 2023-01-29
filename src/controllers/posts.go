e3803b3dd0e788a7c1f726b384e9e97271f23a9b
Junior Paula <luke.junnior@icloud.com>
2023-01-29 15:11:34 -0300

    Implements method to create a new post

--- src/controllers/posts.go
+++ src/controllers/posts.go
@@ -1,8 +1,54 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/auth"
+	"api/src/database"
+	"api/src/models"
+	"api/src/repositories"
+	"api/src/utils"
+	"encoding/json"
+	"io"
+	"net/http"
+)
 
-func CreatePost(w http.ResponseWriter, r *http.Request) {}
+func CreatePost(w http.ResponseWriter, r *http.Request) {
+	userID, err := auth.GetUserID(r)
+	if err != nil {
+		utils.ResponseError(w, http.StatusUnauthorized, err)
+		return
+	}
+
+	requestBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		utils.ResponseError(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	var post models.Post
+	if err = json.Unmarshal(requestBody, &post); err != nil {
+		utils.ResponseError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	post.AuthorID = userID
+
+	db, err := database.Connect()
+	if err != nil {
+		utils.ResponseError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	defer db.Close()
+
+	repository := repositories.NewPostsRepository(db)
+	post.ID, err = repository.Create(post)
+	if err != nil {
+		utils.ResponseError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.ResponseJSON(w, http.StatusCreated, post)
+}
 
 func FindAllPosts(w http.ResponseWriter, r *http.Request) {}
 
