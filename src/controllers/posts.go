e19627766a60ce98dafdb8226a574a4a100f49a6
Junior Paula <luke.junnior@icloud.com>
2023-01-30 08:42:23 -0300

    Implements method to update a post

--- src/controllers/posts.go
+++ src/controllers/posts.go
@@ -7,6 +7,7 @@ import (
 	"api/src/repositories"
 	"api/src/utils"
 	"encoding/json"
+	"errors"
 	"io"
 	"net/http"
 	"strconv"
@@ -107,6 +108,63 @@ func FindPost(w http.ResponseWriter, r *http.Request) {
 	utils.ResponseJSON(w, http.StatusOK, post)
 }
 
-func UpdatePost(w http.ResponseWriter, r *http.Request) {}
+func UpdatePost(w http.ResponseWriter, r *http.Request) {
+	userID, err := auth.GetUserID(r)
+	if err != nil {
+		utils.ResponseError(w, http.StatusUnauthorized, err)
+		return
+	}
+
+	params := mux.Vars(r)
+	postID, err := strconv.ParseUint(params["postId"], 10, 64)
+	if err != nil {
+		utils.ResponseError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		utils.ResponseError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	defer db.Close()
+
+	repository := repositories.NewPostsRepository(db)
+	postFromDB, err := repository.FindById(postID)
+	if err != nil {
+		utils.ResponseError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	if postFromDB.AuthorID != userID {
+		utils.ResponseError(w, http.StatusUnauthorized, errors.New("you are not autorized to update its not your"))
+		return
+	}
+
+	requestBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		utils.ResponseError(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+
+	var post models.Post
+	if err = json.Unmarshal(requestBody, &post); err != nil {
+		utils.ResponseError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err = post.Validations(); err != nil {
+		utils.ResponseError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	if err = repository.Update(postID, post); err != nil {
+		utils.ResponseError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.ResponseJSON(w, http.StatusNoContent, nil)
+}
 
 func DeletePost(w http.ResponseWriter, r *http.Request) {}
